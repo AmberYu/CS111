cea38ad2bc1d0e2daf37c3046aebf24dcac1eb12
QI SHAO <shaoqi19920815@gmail.com>
2016-01-22 23:47:47 -0800

    pipe--tested

--- lab1/lab1b/main.c
+++ lab1/lab1b/main.c
@@ -5,37 +5,20 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <string.h>
-#include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #define no_argument 0
 #define required_argument 1
 #define optional_argument 2
 
-#define RDONLY    'a'
-#define WRONLY    'b'
-#define COMMAND   'c'
-#define VERBOSE   'd'
-#define APPEND    'e'
-#define CLOEXEC   'f'
-#define CREAT     'g'
-#define DIRECTORY 'h'
-#define DSYNC     'i'
-#define EXCL      'j'
-#define NOFOLLOW  'k'
-#define NONBLOCK  'l'
-#define RSYNC     'm'
-#define SYNC      'n'
-#define TRUNC     'o'
-#define RDWR      'p'
-#define PIPE      'q'
-#define WAIT      'r'
-#define CLOSE     's'
-#define ABORT     't'
-#define CATCH     'u'
-#define IGNORE    'v'
-#define DEFAULT   'w'
-#define PAUSE     'x'
+#define RDONLY  'a'
+#define WRONLY  'b'
+#define COMMAND 'c'
+#define VERBOSE 'd'
+#define PIPE    'e'
+#define CREAT     'f'
+#define APPEND    'g'
+#define TRUNC     'h'
 
 int main(int argc, char * argv[])
 {
@@ -45,39 +28,13 @@ int main(int argc, char * argv[])
         {"wronly",  required_argument, 0, WRONLY },
         {"command", required_argument, 0, COMMAND},
         {"verbose", no_argument,       0, VERBOSE},
-        {"append",  no_argument, 0, APPEND },
-        {"cloexec",  no_argument, 0, CLOEXEC },
-        {"creat", no_argument, 0, CREAT},
-        {"directory", no_argument,       0, DIRECTORY},
-        {"dsync",  no_argument, 0, DSYNC },
-        {"excl",  no_argument, 0, EXCL },
-        {"nofollow", no_argument, 0, NOFOLLOW},
-        {"nonblock", no_argument,       0, NONBLOCK},
-        {"rsync",  no_argument, 0, RSYNC },
-        {"sync",  no_argument, 0, SYNC },
-        {"trunc", no_argument, 0, TRUNC},
-        {"rdwr", required_argument, 0, RDWR},
         {"pipe", no_argument, 0, PIPE},
-        {"wait", no_argument, 0, WAIT},
-        {"close", required_argument, 0, CLOSE},
-        {"abort", no_argument, 0, ABORT},
-        {"catch", required_argument, 0, CATCH},
-        {"ignore", required_argument, 0, IGNORE},
-        {"default", required_argument, 0, DEFAULT},
-        {"pause", no_argument, 0, PAUSE},
+        {"creat", no_argument, 0, CREAT},
+        {"trunc", no_argument, 0, TRUNC},
+        {"append",  no_argument, 0, APPEND },
         {0,           0,               0,  0   }  //denote end
     };
-    //keep record of wait information in parent process for future use when come accross wait option
-    struct wait_info
-    {
-        int childPid;
-        char* args[20];
-    };
-    //an array of wait information which is used in wait case to print out exit status and command of each subprocess
-    struct wait_info waits[20];
-    int numSubPro=0;  //number of subprocess
     
-    int oflag=0;   //record the mode of open
     int index;
     int iarg=0;
     int fd;
@@ -88,9 +45,9 @@ int main(int argc, char * argv[])
     int verbose_shown=0;
     int logic_fd=0;
     //map logic fd to real fd
+    
+    int oflag=0;   //record the mode of open
     int fd_vec[argc];
-    //not pipe 0, readend 1, writeend 2
-    int is_pipe[argc];
     while((iarg = getopt_long(argc, argv, "", longopts, &index)) != -1)
     {
         if(verbose_shown==1){
@@ -110,21 +67,6 @@ int main(int argc, char * argv[])
         }
         switch (iarg)
         {
-            case WAIT:
-            {
-                //printf("%d\n",numSubPro);
-                int i;
-                for(i=0;i<numSubPro;i++){
-                    //call wait
-                    int status;
-                    waitpid(waits[i].childPid,&status,0);
-                    printf("%d", WEXITSTATUS(status));
-                    int j;
-                    for(j=0;waits[i].args[j]!=NULL;j++)
-                        printf(" %s", waits[i].args[j]);
-                    printf("\n");
-                }
-            }
             case VERBOSE:
             {
                 verbose_shown=1;
@@ -139,54 +81,25 @@ int main(int argc, char * argv[])
                     // Execute command
                     if(optarg){
                         optind--;
-                        
-                        
-                        //if is readend close the writeend
-                        if(is_pipe[atoi(argv[optind])] == 1)
-                            close(fd_vec[atoi(argv[optind+1])]);
-                        
-                        //if is write end close the read end
-                        if(is_pipe[atoi(argv[optind])] == 2)
-                            close(fd_vec[atoi(argv[optind-1])]);
-                        
-                        //printf("first logistic fd: %s, real fd is:%d\n", argv[optind],fd_vec[atoi(argv[optind])]);
+                        // printf("first logistic fd: %s, real fd is:%d\n", argv[optind],fd_vec[atoi(argv[optind])]);
                         dup2(fd_vec[atoi(argv[optind++])],0);
-                        //printf("second logistic fd: %s, real fd is:%d\n", argv[optind],fd_vec[atoi(argv[optind])]);
-                        
-                        //if is readend close the writeend
-                        if(is_pipe[atoi(argv[optind])] == 1)
-                            close(fd_vec[atoi(argv[optind+1])]);
-                        
-                        //if is write end close the read end
-                        if(is_pipe[atoi(argv[optind])] == 2)
-                            close(fd_vec[atoi(argv[optind-1])]);
-                        
+                        // printf("second logistic fd: %s, real fd is:%d\n", argv[optind],fd_vec[atoi(argv[optind])]);
                         dup2(fd_vec[atoi(argv[optind++])],1);
-                        //printf("third logistic fd: %s, real fd is:%d\n", argv[optind],fd_vec[atoi(argv[optind])]);
-                        
-                        //if is readend close the writeend
-                        if(is_pipe[atoi(argv[optind])] == 1)
-                            close(fd_vec[atoi(argv[optind+1])]);
-                        
-                        //if is write end close the read end
-                        if(is_pipe[atoi(argv[optind])] == 2)
-                            close(fd_vec[atoi(argv[optind-1])]);
-                        
+                        // printf("third logistic fd: %s, real fd is:%d\n", argv[optind],fd_vec[atoi(argv[optind])]);
                         dup2(fd_vec[atoi(argv[optind++])],2);
-                        //optind++;
-                        //optind++;
-                        //printf("optind: %d, argc: %d\n", optind,argc);
                         
+                        // printf("~~~~~~~\n");
                         
-                        // int j = 3;
-                        // int stop = logic_fd + 3;
+                        int j = 0;
+                        // printf("%d\n", logic_fd);
                         
-                        // for(j; j <= stop; ++j)
-                        // {
-                        //     close(fd_vec[j]);
-                        
-                        // }
-                        
+                        for(j; j < logic_fd; ++j)
+                        {
+                            // printf("~~~~~~~\n");
+                            // printf("%d\n", fd_vec[j]);
+                            
+                            close(fd_vec[j]);
+                        }
                         
                         int i;
                         for (i = 0;optind < argc; ++i,++optind)
@@ -209,21 +122,9 @@ int main(int argc, char * argv[])
                 // Parent process
                 else {
                     // Wait for child process to finish
-                    int childStatus;
-                    struct wait_info wait;
-                    wait.childPid = waitpid(-1, &childStatus, 0);
-                    //store argument of this command
-                    int i,arg_idx;
-                    for (i = 0,arg_idx = optind+2;arg_idx < argc; ++i,++arg_idx)
-                    {
-                        if(strncmp(argv[arg_idx], "--", 2) == 0)
-                            break;  //hit the next option
-                        wait.args[i] = argv[arg_idx];
-                        //printf("%s\n", wait.args[i]);
-                    }
-                    wait.args[i] = NULL;
-                    waits[numSubPro++] = wait;
-                    //return 1;
+                    // int childStatus;
+                    // waitpid(pid, &childStatus, 0);
+                    // return 1;
                 }
                 break;
             }
@@ -234,11 +135,9 @@ int main(int argc, char * argv[])
                 // printf("1\n");
                 pipe(pipefd);
                 // printf("1\n");
-                is_pipe[logic_fd] = 1;
                 fd_vec[logic_fd++] = pipefd[0];  //read end of pipe
                 // printf("%d\n", pipefd[0]);
                 // printf("%d\n", logic_fd);
-                is_pipe[logic_fd] = 2;
                 fd_vec[logic_fd++] = pipefd[1];  //write end of pipe
                 // printf("%d\n", pipefd[1]);
                 // printf("%d\n", logic_fd);
@@ -247,68 +146,28 @@ int main(int argc, char * argv[])
                 
             case APPEND:
             {
-                printf("append called\n");
                 oflag|=O_APPEND;
                 break;
             }
-            case CLOEXEC:
-            {
-                oflag|=O_CLOEXEC;
-                break;
-            }
-            case CREAT:
-            {
-                printf("creat called\n");
-                oflag|=O_CREAT;
-                break;
-            }
-            case DIRECTORY:
-            {
-                oflag|=O_DIRECTORY;
-                break;
-            }
-            case DSYNC:
-            {
-                oflag|=O_DSYNC;
-                break;
-            }
-            case EXCL:
-            {
-                oflag|=O_EXCL;
-                break;
-            }
-            case NOFOLLOW:
-            {
-                oflag|=O_NOFOLLOW;
-                break;
-            }
-            case NONBLOCK:
-            {
-                oflag|=O_NONBLOCK;
-                break;
-            }
-                // case RSYNC:
-                // {
-                //   oflag|=O_RSYNC;
-                //   break;
-                // }
                 
-            case SYNC:
-            {
-                oflag|=O_SYNC;
-                break;
-            }
             case TRUNC:
             {
                 oflag|=O_TRUNC;
                 break;
             }
+                
+            case CREAT:
+            {
+                oflag|=O_CREAT;
+                break;
+            }
+                
             case RDONLY:
             {
                 oflag|=O_RDONLY;
                 if((fd = open(optarg, oflag))!=-1){
                     //printf("read only is hit, the logic fd is %d\n",logic_fd);
-                    is_pipe[logic_fd] = 0;
+                    // is_pipe[logic_fd] = 0;
                     fd_vec[logic_fd++] = fd;  //store the true fd and make it can be visited via logic fd
                     
                 }
@@ -326,7 +185,7 @@ int main(int argc, char * argv[])
                 oflag|=O_WRONLY;
                 if((fd = open(optarg, oflag,0644))!=-1){
                     //printf("write only is hit, the logic fd is %d\n",logic_fd);
-                    is_pipe[logic_fd] = 0;
+                    // is_pipe[logic_fd] = 0;
                     fd_vec[logic_fd++] = fd;  //store the true fd and make it can be visited via logic fd
                 }
                 else{
@@ -336,6 +195,7 @@ int main(int argc, char * argv[])
                 oflag=0;
                 break;
             }
+                
         }
     }
     
