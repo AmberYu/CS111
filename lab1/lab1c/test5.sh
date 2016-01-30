14b54f24fa95b62e55783178070669cdcef13c8d
QI SHAO <shaoqi19920815@gmail.com>
2016-01-29 17:16:15 -0800

    3 test cases

--- lab1/lab1c/test5.sh
+++ lab1/lab1c/test5.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 # test5.sh
 
-touch a.txt
-echo aa >> a.txt
-echo bb >> a.txt
-
-
 ./simpsh \
-  --rdonly a.txt \
+  --rdonly test5_input \
   --pipe \
   --pipe \
-  --creat --trunc --wronly c.txt \
-  --creat --append --wronly d.txt \
-  --ignore 11 \
-  --default 11 \
-  --catch 11 \
-  --abort \
+  --creat --trunc --wronly test5_output.txt \
+  --creat --append --wronly test5_error.txt \
   --command 3 5 6 tr A-Z a-z \
   --command 0 2 6 sort \
   --command 1 4 6 cat b - \
---
+++ lab1/lab1c/test6.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#test6.sh
+
+./simpsh \
+  --rdonly test6_input \
+  --pipe \
+  --pipe \
+  --creat --trunc --wronly test6_output.txt \
+  --creat --append --wronly test6_error.txt \
+  --command 0 2 6 cat \
+  --command 1 4 6 grep CS111 \
+  --command 3 5 6 sort \
+  --wait
